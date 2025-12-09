50b284bbb5e27fe8b41fd9c741c7b3ce7081ed1a
Xamacardoso <contato.xcardoso@gmail.com>
2025-12-08 23:02:31 -0300

    :books: Docs: Added swagger API documentation for basic auth endpoints

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ZodValidationPipe } from 'nestjs-zod';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,6 +12,18 @@ async function bootstrap() {
   
   // Ativa a validação de dados com Zod em todos os endpoints
   await app.useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('EventSync API')
+    .setDescription('API for EventSync application, managing events and users.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('Auth')
+    .build();
+
+  // Cria o documento Swagger e disponibiliza em /docs
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
--- src/presentation/controllers/auth.controller.ts
+++ src/presentation/controllers/auth.controller.ts
@@ -1,26 +1,39 @@
 import { AuthService } from "src/application/services/auth.service";
 import { LoginDto, RegisterDto } from "../dtos/auth.dto";
-import { Controller, Get, UseGuards, Post } from "@nestjs/common";
+import { Controller, Get, UseGuards, Post, HttpCode, HttpStatus } from "@nestjs/common";
 import { Body } from "@nestjs/common";
 import { JwtAuthGuard } from "src/infra/auth/jwt-auth.guard";
 import { CurrentUser } from "src/infra/auth/current-user.decorator";
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('register')
+    @ApiOperation({ summary: 'Register a new user' })
+    @ApiResponse({ status: 201, description: 'User registered successfully' })
+    @ApiResponse({ status: 409, description: 'User already exists' })
     async register(@Body() registerDto: RegisterDto) {
         return this.authService.register(registerDto);
     }
 
     @Post('login')
+    @HttpCode(HttpStatus.OK) // Retorna 200 ao invés de 201
+    @ApiOperation({ summary: 'User login, obtaining jwt token' })
+    @ApiResponse({ status: 200, description: 'Login successful, JWT token returned' })
+    @ApiResponse({ status: 401, description: 'Invalid credentials' })
     async login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
     }
 
     @Get('check-token')
     @UseGuards(JwtAuthGuard) // Protege a rota com JWT. So funciona se tiver logado
+    @ApiBearerAuth() // Adiciona o ícone de cadeado no Swagger
+    @ApiOperation({ summary: 'Check if JWT token is valid' })
+    @ApiResponse({ status: 200, description: 'Token is valid' })
+    @ApiResponse({ status: 401, description: 'Unauthorized (invalid or inexistent token)' })
     async checkToken(@CurrentUser() user: any) {
         return { message: 'Token is valid' };
     }
